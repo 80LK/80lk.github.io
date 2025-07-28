15f3a1e14217ebbac479cb635422e42e80c3a87a
80LK <80lkru@gmail.com>
2025-07-28 21:55:01 +0300

    end project page

--- app/pages/[path].vue
+++ app/pages/[path].vue
@@ -7,6 +7,29 @@ console.log(unref(project));
 </script>
 <template>
 	<Container width="720px">
+		<img :class="$style.icon" :src="project?.icon" v-if="project?.icon" />
 		<ContentRenderer :value="project!" />
+		<div :class="$style.block">
+			<badge v-for="tag of project?.tags" :tag="tag" />
+		</div>
+		<div :class="$style.block">
+			<btn v-for="link of project?.links" :icon="link.icon" :text="link.title" :href="link.href"
+				:style="{ backgroundColor: link.color }" />
+		</div>
 	</Container>
 </template>
+
+<style lang="scss" module>
+.icon {
+	width: 250px;
+	float: left;
+	margin-right: 1em;
+	margin-bottom: 1em;
+}
+
+.block {
+	display: flex;
+	gap: .3em;
+	margin-top: .5em;
+}
+</style>
